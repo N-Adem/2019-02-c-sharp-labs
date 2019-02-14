0709af4943caaeb662cc830f9584b67fd6825fa6
Nadi Adem <NAdem@spartaglobal.com>
2019-02-14 06:23:47 +0000

    new random person game has been added

---
+++ labs/Random-name-gen-01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_name_gen_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent p1 = new Parent();
+            Parent p2 = new Parent();
+            p1.Age = 32;
+            p1.Name = "bert";
+            p2.Age = 56;
+            p2.Name = "chase";
+            Console.WriteLine("first parent = "+ p1.Age + " name is = " + p1.Name);
+            Console.WriteLine("second parent = "+ p2.Age + " name is = " + p2.Name);         
+        }
+    }
+    class Parent
+    {
+        int age;
+        string name;
+        string[] nameList;
+        public  string Name{ get { return name;} set { name = value; } }
+        public int Age { get{ return age; } set { age = value; } }      
+        public Parent()
+        {
+
+        }
+      
+    }
+
+}
---
+++ labs/lab_05/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            //GameManager.TimerCounter(20);
+          
+            GamePlay.AddKey();
+        }
+    }
+    class GameManager
+    {
+        // Methods ---------
+        public static void TimerCounter(int value)
+        {
+            float timer = (float)value;
+            while (timer > 0)
+            {
+                timer = timer - (float)0.1;
+                Console.WriteLine(timer);
+                if (timer < 0)
+                {
+                    Console.WriteLine("gameover");
+                    break;
+                }
+            }
+        }
+    }
+    class GamePlay : GameManager
+    {
+        // code for letters in a string to an alphabet in an array of chars. 
+        static string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static char[] alphaLetters = alphabet.ToCharArray();
+
+        // method adds keys entered to an array and checks whether its in the the alphabet array.
+        public static void AddKey()
+        {
+            var inputLet;
+            List<char> letters = new List<char>();
+            for (int i = 0; i < 20; i++)
+            {
+                inputLet = Console.ReadLine().ToString();
+                letters.Add(inputLet.ToCharArray());          
+            }
+            Console.WriteLine("current size of the alphabet size is" + letters.Capacity());
+        }
+        public static void CheckLetter()
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine(alphaLetters.Length);
+            }
+        }
+
+    }
+}
