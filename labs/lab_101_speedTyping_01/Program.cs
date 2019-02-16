13bcd7aca68420a720f1120a382713ccca7d52bf
Nadi Adem <NAdem@spartaglobal.com>
2019-02-16 20:00:47 +0000

    alphabet games complete

--- labs/lab_101_speedTyping_01/Program.cs
+++ labs/lab_101_speedTyping_01/Program.cs
@@ -10,22 +10,44 @@ namespace lab_101_speedTyping_01
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
+            // Game flow variables
+            bool isRand = false;
             bool isReady = false;
-            double endValue = 0;
+            bool isOrdered = false;
             Stopwatch watch = new Stopwatch();
+            // Game Intro 
+            Console.WriteLine("Would you like to play the ordered or unordered (1/2)");
+            var choice = Console.ReadKey().KeyChar;
+            if (choice == 1)
+            {
+                isRand = true;
+                Console.Write("you picked un ordered\n");
+            }
+            if (choice == 2)
+            {
+                isOrdered = true;
+                Console.Write("you picked ordered\n");
+            }
             Console.WriteLine("\nWould you like to start the typing game  (y/n)\n");
             Console.WriteLine("-----------------------------------------");
             char response = Console.ReadKey().KeyChar;
-            if (response == 'y')
+            if (response == 'Y')
             {
                 Console.WriteLine("** Begin Game **");
                 watch.Start();
                 isReady = true;
                 while (watch.Elapsed.TotalSeconds < 10)
                 {
-                    GamePlay.SpeedTypeRand();
-                   // Console.WriteLine("game active");
+                    if(isRand == true)
+                    {
+                        GamePlay.SpeedTypeRand();
+                    }
+                    if (isOrdered == true)
+                    {
+                        GamePlay.SpeedTypeOrdered();
+                    }                   
+                    //Console.WriteLine("game active");
                 }
                 isReady = false;
             }
@@ -33,7 +55,7 @@ namespace lab_101_speedTyping_01
             {
                 Console.WriteLine($"**GAMEOVER** \nTotal time taken is = {watch.Elapsed.TotalSeconds.ToString()}");
             }
-                Console.Read();
+            Console.Read();
         }
     }//-----|End Of Main Method
     class GamePlay
@@ -43,7 +65,31 @@ namespace lab_101_speedTyping_01
 
         public static void SpeedTypeRand()
         {
-            Console.WriteLine("input here please");   
+            typed += Console.ReadKey().KeyChar;
+            Console.WriteLine("input here please");
+        }
+
+        public static void SpeedTypeOrdered()
+        {
+            string val = "";
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            char[] alphaLetters = alphabet.ToCharArray();
+            for (int i = 0; i < alphaLetters.Length;i++)
+            {
+                val += alphaLetters[i].ToString();
+                typed += Console.ReadKey().KeyChar;
+                //Console.WriteLine(val);
+                if (typed == val)
+                {
+                    Console.WriteLine("you may continue");
+                }
+                else
+                {
+                    Console.WriteLine("YOU ENTERED THE WRONG LETTER");
+                }
+
+                //Console.WriteLine(alphaLetters[3]);
+            }
         }
     }//-----| End of GamePlay class
 
